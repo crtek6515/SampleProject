ddc47ad88913a63e9459a5e9c968bca1d3d2299a
Christopher Vance <cvance@christophersair.lan>
2019-09-01 15:25:23 -0400

    copied calc.py over to Git

---
+++ calc.py
@@ -0,0 +1,4 @@
+first = int(input('What is your first number: '))
+second = int(input('What is your second number: '))
+result = first + second
+print(str(first) + "+" + str(second) + "=" + str(result) )
