660d129ed3f5fc369a7f1502856c82db316d4952
Christopher Vance <cvance@christophersair.lan>
2019-09-01 15:16:34 -0400

    added 8 ball to Sample Project

---
+++ 8ball.py
@@ -0,0 +1,35 @@
+import random
+from os import system, name
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is definitely so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy, try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very Doubtful'
+
+def screen_clear():
+    if name =='nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+screen_clear()
+print('The Magic 8 Ball is ready to give you your fortune:')
+r = random.randint(1, 9)
+fortune = getAnswer(r)
+print(fortune)
+print('')
