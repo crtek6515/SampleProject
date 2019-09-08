8777fdaf21d3670b00da5abe4a147625c3fbc52e
Christopher Vance <cvance@christophersair.lan>
2019-09-08 15:04:35 -0400

    added new 8ballList program

---
+++ 8ballList.py
@@ -0,0 +1,25 @@
+import random
+from os import system, name
+
+messages = ['It is certain',
+    'It is decidedly so',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful']
+
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
+clear()
+print(messages[random.randint(0, len(messages) - 1)])
